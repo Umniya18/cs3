be8ae72d22bd6fb1f2a2b1e8737e94723cdc3e1d
Umniya18 <umniyaemam@comcast.net>
2026-09-15 08:39:09 -0500

    changes

--- u02a_arraylists/labs/04c - Histogram/Histogram.java
+++ u02a_arraylists/labs/04c - Histogram/Histogram.java
@@ -1,9 +1,3 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  -
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -15,35 +9,119 @@ public class Histogram
 {
 	private ArrayList<Integer> count;
 	private ArrayList<Character> letters;
-	private String fileName;
-
+	private String gram;
+   private File n;
+   private Scanner scan;
+   
 	public Histogram()
 	{
 	}
 
-	public Histogram(char[] values, String fName)
+	public Histogram(char[] values, String gName)
 	{
+      letters = new ArrayList<Character>();
+      for(char v : values)
+      {
+         letters.add(v);
+      }
 		out.println("search letters = "+letters);
+      gram = gName;
+      int sizer = letters.size();
+      count = new ArrayList<Integer>();
+      for(int i = 0; i < letters.size(); i++)
+      {
+         count.add(0);
+      }
 	}
 
 	public void loadAndAnalyzeFile() throws IOException
 	{
-	}
+      n = new File(gram);
+      scan = new Scanner(n);
+      
+      
+      char a = letters.get(0);
+      int cur = 0;
+      String see = "";
+      int current = 0;
+      int L = 0;
+      
+      for(int k = 0; k < letters.size(); k++)
+      {
+         scan = new Scanner(n);
+         cur = 0;
+         current = k;
+      while(scan.hasNext())
+      {
+         
+         
+            see = scan.next();
+            for(int i = 0; i < see.length(); i++)
+            {
+               char b = see.charAt(i);
+               char c = letters.get(k);
+               if(b == c)
+               {
+                  cur++;
+               
+               }
+            
+            }
+            count.set(current, cur);
+            
+         }
+     
+      }
+      out.println(count);
+
+   }
+	   
+      
 
 	public char mostFrequent()
 	{
-		return '#';
+      
+      int find = count.get(0);
+      int ind = 0;
+      for(int b : count)
+      {
+         if (b > find)
+         {
+            find = b;
+            ind++;
+         }
+      
+      }
+      
+      
+      
+		return letters.get(ind);
+     
 	}
 
 	public char leastFrequent()
 	{
-		return '#';
+      int least = count.get(0);
+      int ind = 0;
+      for(int a : count)
+      {
+         if(a < least)
+         {
+            least = a;
+            ind++;
+         
+         }
+      
+      }
+   
+		return letters.get(ind);
 	}
 
 	public String toString()
 	{
-	   return fileName
+	   return gram
 	   + "\n" + letters
 	   + "\n" + count + "\n\n\n";
 	}
 }
+
