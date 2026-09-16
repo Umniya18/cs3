b8d76f60be127d844c9eb7604a0c3e7a706f2816
Umniya18 <umniyaemam@comcast.net>
2026-09-16 09:01:31 -0500

    finished

--- u02a_arraylists/labs/04b - NumberAnalyzer/NumberTester.java
+++ u02a_arraylists/labs/04b - NumberAnalyzer/NumberTester.java
@@ -11,15 +11,10 @@ public class NumberTester
 	public static void main(String[] args)
 	{
 		//add test cases
-		
-		
-		
-		
-		
-		//add more test cases
-		
-		
-		
+		Number test = new Number(7);
+		out.println(test);
+		out.println(test + " is Odd == " + test.isOdd());
+		out.println(test + " isPerfect == " + test.isPerfect());
 		
 		
 		
